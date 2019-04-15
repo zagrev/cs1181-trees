960814f31e6cefdd48c3c37f0308b50a94a0fc86
Steve Betts <sbetts@minethurn.com>
2019-04-15 18:46:49 -0400

    moved test to correct package

--- cs1181-sp19-trees/test/cs1181/trees/TestBinaryTree.java
+++ cs1181-sp19-trees/test/cs1181/trees/TestBinaryTree.java
@@ -1,3 +1,4 @@
+package cs1181.trees;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
